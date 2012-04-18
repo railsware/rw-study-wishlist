8790d98eaba21e5dd22b32d2c6987bfc8330afd8
Yevhenii Yevtushenko <freeman.zhenia@gmail.com>
2012-04-19 01:49:11 +0300

    Test commit 3

--- config/application.rb
+++ config/application.rb
@@ -48,11 +48,10 @@ module Wishlist
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-<<<<<<< HEAD
+
     config.active_record.whitelist_attributes = true
-=======
     # config.active_record.whitelist_attributes = true
->>>>>>> 71f17942ec3377d04192a4686e20f975517a45d6
+
 
     # Enable the asset pipeline
     config.assets.enabled = true
