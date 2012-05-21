583fdc58c5271b834776f8709851ada053c78dc1
Yevhenii Yevtushenko <freeman.zhenia@gmail.com>
2012-05-21 23:28:58 +0300

    Add some form validations on client side. BETA. Don't pull)

--- app/assets/javascripts/validates.js
+++ app/assets/javascripts/validates.js
@@ -6,8 +6,40 @@
  * To change this template use File | Settings | File Templates.
  */
 $(document).ready(function () {
-    $("p").on(function() {
-        $(this).html("<p>EPTA</p>");
+
+    var emptyError = "Поле не может быть пустым";
+    var urlError = "Некоректный url";
+    var ratingError = "Некоректный рейтинг";
+    var nameMaxLengthError = "Слишком длинное имя";
+    var nameMinLengthError = "Слишком короткое имя";
+    var errorName = "";
+    var errorUrl = "";
+    var errorRating = "";
+
+    $("input#wish_name").blur(function()
+    {
+       if ($(this).value.length == 0)
+       {
+           errorName = emptyError;
+       }
+       else
+       {
+           if ( ($(this).value.length > 0) && ($(this).value.length < 2))
+           {
+               errorName = nameMinLengthError;
+           }
+           else
+           {
+               if ($(this).value.length > 50)
+               {
+                   errorName = nameMaxLengthError;
+               }
+           }
+       }
+
+       $("span#error_name").text(errorName);
+
+
     });
 
 });
