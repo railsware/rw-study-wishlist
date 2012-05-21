97cef4bd20187bb02b688db1442b27d8643724b1
Yevhenii Yevtushenko <freeman.zhenia@gmail.com>
2012-05-21 12:32:33 +0300

    Add some changes

---
+++ app/assets/javascripts/validates.js
@@ -0,0 +1,13 @@
+/**
+ * Created with JetBrains RubyMine.
+ * User: freeman13
+ * Date: 5/21/12
+ * Time: 1:41 AM
+ * To change this template use File | Settings | File Templates.
+ */
+$(document).ready(function () {
+    $("p").on(function() {
+        $(this).html("<p>EPTA</p>");
+    });
+
+});
