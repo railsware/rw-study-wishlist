9c166b8f7ca4e0f7a46c637208847f26201190e9
Bogda Bondar <bobo0.6180@bigmir.net>
2012-05-22 14:03:43 +0300

    update config/initializers/omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # The following is for facebook
-   provider :vkontakte, '2877279', 'Opy78Aqv9BtRkMXGbGJU', :scope => 'friends,wall', :display => 'popup'
+   provider :vkontakte, '2879917', 'sJMsaa7EYkcsh75iKPMe', :scope => 'friends,wall', :display => 'popup'
  
  
   # If you want to also configure for additional login services, they would be configured here.
