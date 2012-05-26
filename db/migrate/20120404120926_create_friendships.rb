fb7473bfa6e4aa4a1967add9dcd9cba23107b951
kapitoshka <andropovyuriy@gmail.com>
2012-05-26 17:15:44 +0300

    re fix encoding migrations

--- db/migrate/20120404120926_create_friendships.rb
+++ db/migrate/20120404120926_create_friendships.rb
@@ -1,6 +1,6 @@
 class CreateFriendships < ActiveRecord::Migration
   def change
-    create_table :friendships do |t|
+    create_table(:friendships, :options => 'ENGINE=InnoDB DEFAULT CHARSET=utf8') do |t|
       t.string :person_id
       t.string :friend_id
 
