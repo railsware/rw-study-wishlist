e51e2b9c2370c15132c40be8f59d4c0c51755131
Bogda Bondar <bobo0.6180@bigmir.net>
2012-06-05 22:53:09 +0300

    pull only medium_rec avatars

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -3,7 +3,7 @@ class LandingController < ApplicationController
     @title = "Welcome"
 	  auth_hash = request.env['omniauth.auth']
 	  @app = VkontakteApi::Client.new(auth_hash.credentials.token)
-	  friends_hashes = @app.friends.get(fields: 'uid, first_name, last_name, bdate, photo_medium, photo_medium_rec, photo_big')
+	  friends_hashes = @app.friends.get(fields: 'uid, first_name, last_name, bdate, photo_medium_rec')
 	  @person = Person.find_for_vkontakte_oauth auth_hash, friends_hashes
 
     if @person.persisted?
