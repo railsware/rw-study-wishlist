6f10a395c5beed84296f8f2e75dcb7d9d5defeb6
Yevhenii Yevtushenko <freeman.zhenia@gmail.com>
2012-06-17 15:50:09 +0300

    Add missing titles

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -12,11 +12,13 @@ class FriendsController < ApplicationController
   public
 
   def index
+    @title ||= "All your VK friends"
     val = current_user.friendships
     @friends = val.collect { |v| v.friend }
   end
   
   def bdate_in_1_week
+    @title = "Friends who have birthday in 1 week"
     d_now = DateTime.now
     friends = index
     @friends_1_week = friends.collect { |v| if v.birthday != nil then if (v.birthday.month == d_now.month)&&(v.birthday.day - d_now.day <= 7)&&(v.birthday.day - d_now.day >= 0) then v end end }
@@ -24,6 +26,7 @@ class FriendsController < ApplicationController
   end
   
   def bdate_in_2_weeks
+    @title = "Friends who have birthday in 2 weeks"
     d_now = DateTime.now
     friends = index
     @friends_2_weeks = friends.collect { |v| if v.birthday != nil then if (v.birthday.month == d_now.month)&&(v.birthday.day - d_now.day <= 14)&&(v.birthday.day - d_now.day >= 0) then v end end }
@@ -31,6 +34,7 @@ class FriendsController < ApplicationController
   end
   
   def bdate_in_1_month
+    @title = "Friends who have birthday in 1 month"
     d_now = DateTime.now
     friends = index
     @friends_1_month = friends.collect { |v| if v.birthday != nil then if ((v.birthday.month == d_now.month)&&(v.birthday.day - d_now.day >= 0))||((v.birthday.month == d_now.month + 1)&&(d_now.day - v.birthday.day >= 0)) then v end end }
@@ -38,6 +42,7 @@ class FriendsController < ApplicationController
   end
   
   def bdate_unknown
+    @title = "Friends who have birthday and we don't know when"
     d_now = DateTime.now
     friends = index
     @friends_unknown = friends.collect { |v| if v.birthday == nil then v end }
@@ -47,6 +52,7 @@ class FriendsController < ApplicationController
   def show
     #d_now = DateTime.now
     @friend = Person.find(params[:id])
+    @title = "Page by #{@friend.name}"
     @wishes = Wish.where(:owner_id => params[:id])
     #@days_till_bday = Person.find(params[:id]).birthday - d.now
   end
