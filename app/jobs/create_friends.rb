a27fa87e492fb6f6bd9d63d0fbde627f4aca24e9
Bogda Bondar <bobo0.6180@bigmir.net>
2012-07-04 15:51:34 +0300

    add create_friends background job

--- app/jobs/create_friends.rb
+++ app/jobs/create_friends.rb
@@ -1,7 +1,11 @@
 class CreateFriends
  @queue = :simple
 
- def self.perform
+ def self.perform(person_id, token)
+    @app = VkontakteApi::Client.new(token)
+	friends_hashes = @app.friends.get(fields: 'uid, first_name, last_name, bdate, photo_medium_rec')
+	person = Person.find(person_id)
+	Person.create_friends friends_hashes, person
     puts "CreatFriends is done"
  end
 end
