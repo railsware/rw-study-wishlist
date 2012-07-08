1f7c6bf46bfed682feb59cc240d46f0487fd6b80
Bogda Bondar <bobo0.6180@bigmir.net>
2012-07-08 15:14:27 +0300

    config deploy for resque 8

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -12,6 +12,6 @@
 #resque_config = YAML.load_file(rails_root + '/config/resque.yml')
 #Resque.redis = resque_config[rails_env]
 
-#uri = URI.parse("redis://localhost:6379/")  
-#Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+uri = URI.parse("redis://redis1.ae.github.com:6379/")  
+Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
