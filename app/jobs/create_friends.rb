bdf8ddf23a9719edea5d89d5b2ed7238b4ca751b
Bogda Bondar <bobo0.6180@bigmir.net>
2012-07-22 13:30:28 +0300

    config deploy for resque 13

--- app/jobs/create_friends.rb
+++ app/jobs/create_friends.rb
@@ -2,10 +2,14 @@ class CreateFriends
  @queue = :simple
 
  def self.perform(person_id, token)
+ 	puts "CreatFriends: started"
     @app = VkontakteApi::Client.new(token)
+    puts "CreatFriends: VK api client initialize successful"
 	friends_hashes = @app.friends.get(fields: 'uid, first_name, last_name, bdate, photo_medium_rec')
 	person = Person.find(person_id)
 	Person.create_friends friends_hashes, person
-    puts "CreatFriends is done"
+	puts "CreatFriends: model method 'create_friends' worked successful"
+    puts "CreatFriends: finished"
+    puts "--------------------------------------------------------------"
  end
 end
--- app/jobs/update_friends.rb
+++ app/jobs/update_friends.rb
@@ -2,6 +2,7 @@ class UpdateFriends
  @queue = :simple
 
  def self.perform(person_id, token)
+    puts "UpdateFriends: started"
     person = Person.find(person_id)
     friends = person.friendships.collect {|f| f.friend}
     b = false
@@ -9,16 +10,19 @@ class UpdateFriends
     	d_updated = DateTime.strptime(f.updated_at.strftime('%d.%m.%y %H:%M:%S'), '%d.%m.%y %H:%M:%S')
     	d_now = DateTime.strptime(DateTime.now.strftime('%d.%m.%y %H:%M:%S'), '%d.%m.%y %H:%M:%S')
     	if (d_now - d_updated).to_int > 0
-    	puts "UpdateFriends is done 1"
-    	@app = VkontakteApi::Client.new(token)
-    	puts "UpdateFriends is done 2"
-    	friends_hashes = @app.friends.get(fields: 'uid, first_name, last_name, bdate, photo_medium_rec')
-    	puts "UpdateFriends is done 3"
-    	Person.update_friends friends_hashes, person
-    	puts "UpdateFriends is done 4"
-    	return
+    		@app = VkontakteApi::Client.new(token)
+    		puts "UpdateFriends: VK api client initialize successful"
+    		friends_hashes = @app.friends.get(fields: 'uid, first_name, last_name, bdate, photo_medium_rec')
+    		puts "UpdateFriends: get friends from api successful"
+    		Person.update_friends friends_hashes, person
+    		puts "UpdateFriends: model method 'update_friends' worked successful"
+    		puts "UpdateFriends: finished"
+    		puts "--------------------------------------------------------------"
+    		return
     	end
     end   
-    puts "UpdateFriends is done without http request"
+    puts "UpdateFriends: finished without http request"
+    puts "--------------------------------------------------------------"
+
  end
 end
