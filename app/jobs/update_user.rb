34056b2fa09da2e307dcbcd34c5432e403c74ff3
Bogda Bondar <bobo0.6180@bigmir.net>
2012-07-22 10:37:56 +0300

    Revert "fix problem with destroying friendships"
    This reverts commit 51a185e5bf8bb107d2bc28d7607c0d6452c54beb.

--- app/jobs/update_user.rb
+++ app/jobs/update_user.rb
@@ -2,7 +2,6 @@ class UpdateUser
  @queue = :simple
 
  def self.perform(person_id, vk_id, token)
- 	puts "UpdateUser: started"
     person = Person.find(person_id) 
     @app = VkontakteApi::Client.new(token)
     current_user_hash = @app.users.get(uids: vk_id, fields: 'bdate,photo_medium_rec').first
@@ -18,7 +17,6 @@ class UpdateUser
 	     						     	DateTime.strptime(current_user_hash[:bdate], '%d.%m')
 	     						 	  end 
 	     						 	end)
-    puts "UpdateUser: finished"
-    puts "--------------------------------------------------------------"
+    puts "UpdateUser is done"
  end
 end
