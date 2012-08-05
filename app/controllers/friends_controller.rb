e402640ae77cb19a3046ce20434f2242d30c9b81
Bogda Bondar <bobo0.6180@bigmir.net>
2012-08-05 22:04:20 +0300

    modified friends_controller

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -13,16 +13,14 @@ class FriendsController < ApplicationController
   public
 
   def index
-    @title ||= "All your VK friends"
-    val = current_user.friendships
-    @friends = val.collect { |v| v.friend }
+    @friends = current_user.friends
     @friends = @friends.sort_by { |hsh| [hsh[:is_user] ? 0:1]}
   end
   
   def bdate_in_1_week
     @title = "Friends who have birthday in 1 week"
     d_now = DateTime.now
-    friends = index
+    friends = current_user.friends
     @friends_1_week = friends.collect { |v| if v.birthday != nil then if (v.birthday.month == d_now.month)&&(v.birthday.day - d_now.day <= 7)&&(v.birthday.day - d_now.day >= 0) then v end end }
     @friends_1_week.delete(nil)
     @friends = @friends.sort_by { |hsh| [hsh[:is_user] ? 0:1]}  
@@ -31,7 +29,7 @@ class FriendsController < ApplicationController
   def bdate_in_2_weeks
     @title = "Friends who have birthday in 2 weeks"
     d_now = DateTime.now
-    friends = index
+    friends = current_user.friends
     @friends_2_weeks = friends.collect { |v| if v.birthday != nil then if (v.birthday.month == d_now.month)&&(v.birthday.day - d_now.day <= 14)&&(v.birthday.day - d_now.day >= 0) then v end end }
     @friends_2_weeks.delete(nil)
     @friends_2_weeks = @friends_2_weeks.sort_by { |hsh| [hsh[:is_user] ? 0:1]}
@@ -40,7 +38,7 @@ class FriendsController < ApplicationController
   def bdate_in_1_month
     @title = "Friends who have birthday in 1 month"
     d_now = DateTime.now
-    friends = index
+    friends = current_user.friends
     @friends_1_month = friends.collect { |v| if v.birthday != nil then if ((v.birthday.month == d_now.month)&&(v.birthday.day - d_now.day >= 0))||((v.birthday.month == d_now.month + 1)&&(d_now.day - v.birthday.day >= 0)) then v end end }
     @friends_1_month.delete(nil)
     @friends_1_month = @friends_1_month.sort_by { |hsh| [hsh[:is_user] ? 0:1]}
@@ -49,7 +47,7 @@ class FriendsController < ApplicationController
   def bdate_unknown
     @title = "Friends who have birthday and we don't know when"
     d_now = DateTime.now
-    friends = index
+    friends = current_user.friends
     @friends_unknown = friends.collect { |v| if v.birthday == nil then v end }
     @friends_unknown.delete(nil)
     @friends_unknown = @friends_unknown.sort_by { |hsh| [hsh[:is_user] ? 0:1]}
